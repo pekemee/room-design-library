92fab1a212ddddcf2313e58daab198760d99cfdb
ken <riyoniriverplus2@yahoo.co.jp>
2020-02-12 11:39:51 +0900

    change carrier

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,12 +4,12 @@ if Rails.env.production?
         config.fog_credentials = {
         # Configuration for Amazon S3
         :provider              => 'AWS',
-        :aws_access_key_id     => "AKIAYR5D5LBJQYSQEHNU",
-        :aws_secret_access_key => "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",
-        :region                => "ap-northeast-1",
+        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+        :region                => ENV['S3_REGION'],
   }
     config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
-    config.fog_directory  = "rdlibrary"
+    config.fog_directory  = ENV['S3_BUCKET']
     config.fog_public     = true
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
    end
