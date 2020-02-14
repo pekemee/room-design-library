03b389496dd8c7a1ca775cf9840b1e2bae719ca8
ken <riyoniriverplus2@yahoo.co.jp>
2020-02-15 01:27:47 +0900

    revive user con

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,12 +37,10 @@ class UsersController < ApplicationController
   end
 
   def new
-    redirect_to root_url
     @user = User.new
   end
 
   def create
-    redirect_to root_url
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
@@ -54,11 +52,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    redirect_to root_url
   end
 
   def update
-    redirect_to root_url
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
